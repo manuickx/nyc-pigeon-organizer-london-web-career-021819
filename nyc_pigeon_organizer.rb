acac04036cf2226061838aee47d254a263f8805d
Manu Ticusan <manuelticusan@yahoo.com>
2019-02-13 18:17:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  
+new = {}
+  data.each do |key, values|
+    values.each do |attrib, names|
+      names.each do |name|
+        if !new.has_key?(name)
+          new[name] = {}
+        end
+        if !new[name].has_key?(key)
+          new[name][key]=[]
+        end
+        if !new[name][key].include?(attrib)
+          new[name][key] << attrib.to_s
+        end
+      end
+    end
+  end
+new
 end
